ca49f23c466d1b60ceff33e5b0fae8f122db3bc5
Baptiste Daniellou <b.daniellou@orange.fr>
2018-05-16 11:31:16 -0400

    Need more CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  resources :restaurants, only: [:create, :index, :show, :new] do
+    resources :reviews, only: [:index, :show, :new, :create]
+  end
+
+  # namespace :admin do
+  #   resources :restaurants do
+  #     resources :reviews
+  #   end
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
